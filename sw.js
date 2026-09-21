d891812ad3344f81799572ee35b5985ba732400b
Mood <64140860+Mood2020@users.noreply.github.com>
2026-09-21 23:26:06 +0330

    feat: publish phase two resource workspace UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "orbit-seo-v9";
+const CACHE = "orbit-seo-v10";
 const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./assets/icon.svg"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
